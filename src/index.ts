1c32d7f55fd8cc010a8e232ff1ce100c2739b215
Holly Skinner <hollyskinner97@gmail.com>
2025-04-07 12:22:46 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import taskRoutes from "./routes/tasks";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 
